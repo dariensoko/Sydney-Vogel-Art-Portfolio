756388f8ad7b03c97d60a03df8c4b4a701ba6625
dariensoko <dariensokoloski@outlook.com>
2017-06-18 19:15:41 -0400

    Modified Background header

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "400px";
+    document.getElementById("mySidenav").style.width = "200px";
 }
 
 /* Set the width of the side navigation to 0 */
